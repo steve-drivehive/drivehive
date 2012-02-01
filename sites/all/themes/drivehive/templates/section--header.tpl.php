077c72ee0794a0eb7b75b5b3d5b1e73c40861478
Jordan Austin <jordotech@Jordan-Austins-MacBook-Pro.local>
2012-02-01 16:57:16 -0600

    more theming

---
+++ sites/all/themes/drivehive/templates/section--header.tpl.php
@@ -0,0 +1,14 @@
+<header<?php print $attributes; ?>>
+<div id="baner-part2">jarjar5
+	<div id="baner-images">
+		<ul>
+			<li id="no1">
+				<img src="img/temp/baner-text.png" alt="" class="fake-text"/>
+			</li>
+		</ul>
+	</div><!-- /baner-images -->
+	<div id="baner-contents">
+  <?php print $content; ?>
+	</div><!-- /baner-contents -->
+</div><!-- /baner-part2 -->
+</header>
